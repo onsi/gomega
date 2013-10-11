7f0b3629de6f23d6fdea81f790d6210dcb913792
Pieter Joost van de Sande <pj@born2code.net>
2013-10-11 08:33:54 +0200

    Prevent panic on comparing nil values

--- matchers/fits_type_matcher.go
+++ matchers/fits_type_matcher.go
@@ -1,14 +1,28 @@
 package matchers
 
 import (
+	"fmt"
 	"reflect"
 )
+
+type FooInsterface interface {
+	Foo()
+}
+
+type ConcreteTypeThatImplementsFooInterface struct{}
+
+func (c *ConcreteTypeThatImplementsFooInterface) Foo() {
+}
 
 type FitsTypeMatcher struct {
 	Expected interface{}
 }
 
 func (matcher *FitsTypeMatcher) Match(actual interface{}) (success bool, message string, err error) {
+	if actual == nil || matcher.Expected == nil {
+		return false, "", fmt.Errorf("Refusing to compare <nil> to <nil>.")
+	}
+
 	actualType := reflect.TypeOf(actual)
 	expectedType := reflect.TypeOf(matcher.Expected)
 
--- matchers/fits_type_matcher_test.go
+++ matchers/fits_type_matcher_test.go
@@ -3,14 +3,31 @@ package matchers_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/matchers"
 )
+
+type ToEmbed struct {
+}
+
+type Embeder struct {
+	ToEmbed
+}
 
 var _ = Describe("FitsType", func() {
 	Context("When asserting equality between types", func() {
 		It("should do the right thing", func() {
-			Ω(0).Should(FitTypeOf(0))
-			Ω(5).Should(FitTypeOf(-1))
-			Ω("foo").Should(FitTypeOf("bar"))
+			Ω(0).Should(FitTypeOf(0))         // Same values
+			Ω(5).Should(FitTypeOf(-1))        // different values same type
+			Ω("foo").Should(FitTypeOf("bar")) // different values same type
+		})
+	})
+
+	Context("When asserting nil values", func() {
+		It("should error", func() {
+			success, _, err := (&FitsTypeMatcher{Expected: nil}).Match(nil)
+
+			Ω(success).Should(BeFalse())
+			Ω(err).Should(HaveOccured())
 		})
 	})
 })
