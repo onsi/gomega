9c9ccc454963763e1f5d593bd7beaeca7bf98a3c
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2013-12-05 18:43:31 -0800

    added documenation for using gomega with Xunit tests

--- testing_t_support.go
+++ testing_t_support.go
@@ -4,13 +4,34 @@ import (
 	"regexp"
 	"runtime/debug"
 	"strings"
-	"testing"
 )
 
+//A simple *testing.T interface wrapper
+type GomegaTestingT interface {
+	Errorf(format string, args ...interface{})
+}
+
 //RegisterTestingT connects Gomega to Golang's XUnit style
-//Testing.T tests.  You'll need to call this at the top of each Testing test
-func RegisterTestingT(t *testing.T) {
-	globalFailHandler = func(message string, callerSkip ...int) {
+//Testing.T tests.  You'll need to call this at the top of each XUnit style test:
+//
+// func TestFarmHasCow(t *testing.T) {
+//     RegisterTestingT(t)
+//
+//	   f := farm.New([]string{"Cow", "Horse"})
+//     Expect(f.HasCow()).To(BeTrue(), "Farm should have cow")
+// }
+//
+// Note that this *testing.T is registered *globally* by Gomega (this is why you don't have to
+// pass `t` down to the matcher itself).  This means that you cannot run the XUnit style tests
+// in parallel as the global fail handler cannot point to more than one testing.T at a time.
+//
+// (As an aside: Ginkgo gets around this limitation by running parallel tests in different *processes*).
+func RegisterTestingT(t GomegaTestingT) {
+	globalFailHandler = buildTestingTOmegaFailHandler(t)
+}
+
+func buildTestingTOmegaFailHandler(t GomegaTestingT) OmegaFailHandler {
+	return func(message string, callerSkip ...int) {
 		skip := 1
 		if len(callerSkip) > 0 {
 			skip = callerSkip[0]
