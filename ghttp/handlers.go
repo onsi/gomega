cf4ac12412b289930993a644461a2ea78d869375
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2014-04-21 09:01:26 -0700

    document ghttp

--- ghttp/handlers.go
+++ ghttp/handlers.go
@@ -4,11 +4,13 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	. "github.com/onsi/gomega"
 	"io/ioutil"
 	"net/http"
+	. "github.com/onsi/gomega"
 )
 
+//CombineHandler takes variadic list of handlers and produces one handler
+//that calls each handler in order.
 func CombineHandlers(handlers ...http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		for _, handler := range handlers {
@@ -17,6 +19,8 @@ func CombineHandlers(handlers ...http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+//VerifyRequest returns a handler that verifies that a request uses the specified method to connect to the specified path
+//You may also pass in an optional rawQuery string which is tested against the request's `req.URL.RawQuery`
 func VerifyRequest(method string, path string, rawQuery ...string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		Ω(req.Method).Should(Equal(method), "Method mismatch")
@@ -27,12 +31,16 @@ func VerifyRequest(method string, path string, rawQuery ...string) http.HandlerF
 	}
 }
 
+//VerifyContentType returns a handler that verifies that a request has a Content-Type header set to the
+//specified value
 func VerifyContentType(contentType string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		Ω(req.Header.Get("Content-Type")).Should(Equal(contentType))
 	}
 }
 
+//VerifyBasicAuth returns a handler that verifies the request contains a BasicAuth Authorization header
+//matching the passed in username and password
 func VerifyBasicAuth(username string, password string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		auth := req.Header.Get("Authorization")
@@ -43,6 +51,11 @@ func VerifyBasicAuth(username string, password string) http.HandlerFunc {
 	}
 }
 
+//VerifyHeader returns a handler that verifies the request contains the passed in headers.
+//The passed in header keys are first canonicalized via http.CanonicalHeaderKey.
+//
+//The request must contain *all* the passed in headers, but it is allowed to have additional headers
+//beyond the passed in set.
 func VerifyHeader(header http.Header) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		for key, values := range header {
@@ -52,6 +65,10 @@ func VerifyHeader(header http.Header) http.HandlerFunc {
 	}
 }
 
+//VerifyJSON returns a handler that verifies that the body of the request is a valid JSON representation
+//matching the passed in JSON string.  It does this using Gomega's MatchJSON method
+//
+//VerifyJSON also verifies that the request's content type is application/json
 func VerifyJSON(expectedJSON string) http.HandlerFunc {
 	return CombineHandlers(
 		VerifyContentType("application/json"),
@@ -64,6 +81,9 @@ func VerifyJSON(expectedJSON string) http.HandlerFunc {
 	)
 }
 
+//VerifyJSONRepresenting is similar to VerifyJSON.  Instead of taking a JSON string, however, it
+//takes an arbitrary JSON-encodable object and verifies that the requests's body is a JSON representation
+//that matches the object
 func VerifyJSONRepresenting(object interface{}) http.HandlerFunc {
 	data, err := json.Marshal(object)
 	Ω(err).ShouldNot(HaveOccurred())
@@ -73,6 +93,7 @@ func VerifyJSONRepresenting(object interface{}) http.HandlerFunc {
 	)
 }
 
+//RespondWith returns a handler that responds to a request with the specified status code and body
 func RespondWith(statusCode int, body string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(statusCode)
@@ -80,6 +101,10 @@ func RespondWith(statusCode int, body string) http.HandlerFunc {
 	}
 }
 
+//RespondWithPtr returns a handler that responds to a request with the specified status code and body
+//
+//Unlike RespondWith, you pass RepondWithPtr a pointer to the status code and body allowing different tests
+//to share the same setup but specify different status codes and bodies.
 func RespondWithPtr(statusCode *int, body *string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(*statusCode)
@@ -87,12 +112,19 @@ func RespondWithPtr(statusCode *int, body *string) http.HandlerFunc {
 	}
 }
 
+//RespondWithJSONEncoded returns a handler that responds to a request with the specified status code and a body
+//containing the JSON-encoding of the passed in object
 func RespondWithJSONEncoded(statusCode int, object interface{}) http.HandlerFunc {
 	data, err := json.Marshal(object)
 	Ω(err).ShouldNot(HaveOccurred())
 	return RespondWith(statusCode, string(data))
 }
 
+//RespondWithJSONEncodedPtr behaves like RespondWithJSONEncoded but takes a pointer
+//to a status code and object.
+//
+//This allows different tests to share the same setup but specify different status codes and JSON-encoded
+//objects.
 func RespondWithJSONEncodedPtr(statusCode *int, object *interface{}) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		data, err := json.Marshal(*object)
