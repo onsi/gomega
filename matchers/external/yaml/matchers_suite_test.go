46bddedbfe8a55d58c3682c80db8edbf98d92a03
Jatin Naik <jnaik@pivotal.io>
2016-03-24 16:09:07 +0000

    added yaml matcher as an 'external' matcher
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

---
+++ matchers/external/yaml/matchers_suite_test.go
@@ -0,0 +1,13 @@
+package yaml_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestMatchers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "YAML Matcher Suite")
+}
