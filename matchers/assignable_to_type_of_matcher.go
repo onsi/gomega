ae301ab5abb39b37507dbe70af31c33abf8fe7dd
Nick Mahoney <nmahoney@pivotal.io>
2018-05-10 13:51:35 -0700

    AssignableToTypeOf returns false without error
    if exactly one of actual or expected is nil.
    
    Fixes #280.
    
    Signed-off-by: Rowan Jacobs <rojacobs@pivotal.io>

--- matchers/assignable_to_type_of_matcher.go
+++ matchers/assignable_to_type_of_matcher.go
@@ -12,8 +12,10 @@ type AssignableToTypeOfMatcher struct {
 }
 
 func (matcher *AssignableToTypeOfMatcher) Match(actual interface{}) (success bool, err error) {
-	if actual == nil || matcher.Expected == nil {
+	if actual == nil && matcher.Expected == nil {
 		return false, fmt.Errorf("Refusing to compare <nil> to <nil>.\nBe explicit and use BeNil() instead.  This is to avoid mistakes where both sides of an assertion are erroneously uninitialized.")
+	} else if actual == nil || matcher.Expected == nil {
+		return false, nil
 	}
 
 	actualType := reflect.TypeOf(actual)
