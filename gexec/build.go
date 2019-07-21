0001ed9df8e1f6dccd9f7f31c51e2aafc087a84a
Christian Muehlhaeuser <muesli@gmail.com>
2019-07-21 16:37:38 +0200

    Simplify code (#356)
    Use simpler string concatenation.

--- gexec/build.go
+++ gexec/build.go
@@ -68,7 +68,7 @@ func doBuild(gopath, packagePath string, env []string, args ...string) (compiled
 
 	executable := filepath.Join(tmpDir, path.Base(packagePath))
 	if runtime.GOOS == "windows" {
-		executable = executable + ".exe"
+		executable += ".exe"
 	}
 
 	cmdArgs := append([]string{"build"}, args...)
