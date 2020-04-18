f22b9157bb23252230344f3f85ea3930def1d155
James Harris <mailjamesharris@gmail.com>
2020-04-18 10:33:26 +1000

    Produce more descriptive failure messages when PanicWith() fails.

--- matchers/panic_matcher_test.go
+++ matchers/panic_matcher_test.go
@@ -52,43 +52,112 @@ var _ = Describe("Panic", func() {
 })
 
 var _ = Describe("PanicWith", func() {
-	When("passed a function of the correct type", func() {
-		It("should call the function and pass if the function panics with the expected value", func() {
-			Expect(func() { panic("ack!") }).To(PanicWith("ack!"))
-			Expect(func() {}).NotTo(PanicWith("ack!"))
+	When("a specific panic value is expected", func() {
+		matcher := PanicWith("ack!")
+
+		When("no panic occurs", func() {
+			actual := func() {}
+
+			It("prints a message that includes the expected value", func() {
+				failuresMessages := InterceptGomegaFailures(func() {
+					Expect(actual).To(matcher)
+				})
+				Expect(failuresMessages).To(ConsistOf(
+					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic with\\s+<string>: ack!"),
+				))
+			})
+
+			It("passes when negated", func() {
+				Expect(actual).NotTo(matcher)
+			})
+		})
+
+		When("the panic value matches", func() {
+			actual := func() { panic("ack!") }
+
+			It("passes", func() {
+				Expect(actual).To(matcher)
+			})
+
+			It("prints a message that includes the (un)expected value when negated", func() {
+				failuresMessages := InterceptGomegaFailures(func() {
+					Expect(actual).NotTo(matcher)
+				})
+				Expect(failuresMessages).To(ConsistOf(
+					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nnot to panic with\\s+<string>: ack!"),
+				))
+			})
+		})
+
+		When("the panic value does not match", func() {
+			actual := func() { panic("unexpected!") }
+
+			It("prints a message that includes both the actual and expected values", func() {
+				failuresMessages := InterceptGomegaFailures(func() {
+					Expect(actual).To(matcher)
+				})
+				Expect(failuresMessages).To(ConsistOf(
+					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic with\\s+<string>: ack!\nbut panicked with\n\\s+<string>: unexpected!"),
+				))
+			})
+
+			It("passes when negated", func() {
+				Expect(actual).NotTo(matcher)
+			})
 		})
 	})
 
-	When("assertion fails", func() {
-		It("prints simple message when positive", func() {
-			failuresMessages := InterceptGomegaFailures(func() {
-				Expect(func() {}).To(PanicWith("ack!"))
-			})
-			Expect(failuresMessages).To(ConsistOf(MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic with\\s+<string>: ack!")))
-		})
+	When("the expected value is actually a matcher", func() {
+		matcher := PanicWith(MatchRegexp("ack"))
 
-		It("prints simple message when negative", func() {
-			failuresMessages := InterceptGomegaFailures(func() {
-				Expect(func() { panic("ack!") }).NotTo(PanicWith("ack!"))
-			})
-			Expect(failuresMessages).To(ConsistOf(MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nnot to panic with\\s+<string>: ack!")))
-		})
+		When("no panic occurs", func() {
+			actual := func() {}
 
-		When("the expected value is actually a matcher", func() {
-			It("prints simple message when positive", func() {
+			It("prints a message that includes the expected value", func() {
 				failuresMessages := InterceptGomegaFailures(func() {
-					Expect(func() {}).To(PanicWith(Equal("ack!")))
-				})
-				Expect(failuresMessages).To(ConsistOf(MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic with a value matching\n.+EqualMatcher.+ack!")))
-			})
-
-			It("prints the object passed to panic() when negative", func() {
-				failuresMessages := InterceptGomegaFailures(func() {
-					Expect(func() { panic("ack!") }).NotTo(PanicWith(MatchRegexp("ack")))
+					Expect(actual).To(matcher)
 				})
 				Expect(failuresMessages).To(ConsistOf(
-					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nnot to panic with a value matching\n.+MatchRegexpMatcher.+ack.+\nbut panicked with\n    <string>: ack!"),
+					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic with a value matching\n.+MatchRegexpMatcher.+ack"),
 				))
+			})
+
+			It("passes when negated", func() {
+				Expect(actual).NotTo(matcher)
+			})
+		})
+
+		When("the panic value matches", func() {
+			actual := func() { panic("ack!") }
+
+			It("passes", func() {
+				Expect(actual).To(matcher)
+			})
+
+			It("prints a message that includes the (un)expected value when negated", func() {
+				failuresMessages := InterceptGomegaFailures(func() {
+					Expect(actual).NotTo(matcher)
+				})
+				Expect(failuresMessages).To(ConsistOf(
+					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nnot to panic with a value matching\n.+MatchRegexpMatcher.+ack"),
+				))
+			})
+		})
+
+		When("the panic value does not match", func() {
+			actual := func() { panic("unexpected!") }
+
+			It("prints a message that includes both the actual and expected values", func() {
+				failuresMessages := InterceptGomegaFailures(func() {
+					Expect(actual).To(matcher)
+				})
+				Expect(failuresMessages).To(ConsistOf(
+					MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic with a value matching\n.+MatchRegexpMatcher.+ack.+\nbut panicked with\n\\s+<string>: unexpected!"),
+				))
+			})
+
+			It("passes when negated", func() {
+				Expect(actual).NotTo(matcher)
 			})
 		})
 	})
