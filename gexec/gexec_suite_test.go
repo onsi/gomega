47c613f37fc33d9a64c02a40f52cad5fe3a2f974
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2021-02-25 19:35:47 +0100

    feat(gexec) Add CompileTest functions. Close #410 (#411)
    * feat(gexec) Add CompileTest functions. Close #410
    
    Adds following methods:
    - CompileTest(packagePath string, args ...string) (compiledPath string, err error)
    - CompileTestWithEnvironment(packagePath string, env []string, args ...string) (compiledPath string, err error)
    - CompileTestIn(gopath string, packagePath string, args ...string) (compiledPath string, err error)
    
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>
    
    * fix(gexec) Binaries for tests are built when needed
    
    This solve the issue in test where suite scoped firefly binary
    was removed by CleanupBuildArtifacts() in build_test.go
    
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>
    
    * feat(gexec) Add methods to get and compile tests
    
    Adds following methods:
    - GetAndCompileTest(packagePath string, args ...string) (compiledPath string, err error)
    - GetAndCompileTestWithEnvironment(packagePath string, env []string, args ...string) (compiledPath string, err error)
    - GetAndCompileTestIn(gopath string, packagePath string, args ...string) (compiledPath string, err error)
    
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

--- gexec/gexec_suite_test.go
+++ gexec/gexec_suite_test.go
@@ -8,15 +8,7 @@ import (
 	"testing"
 )
 
-var fireflyPath string
-
 func TestGexec(t *testing.T) {
-	BeforeSuite(func() {
-		var err error
-		fireflyPath, err = gexec.Build("./_fixture/firefly")
-		Expect(err).ShouldNot(HaveOccurred())
-	})
-
 	AfterSuite(func() {
 		gexec.CleanupBuildArtifacts()
 	})
