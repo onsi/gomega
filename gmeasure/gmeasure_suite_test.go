8c54344b7bc6a1091fe7f1b674ae1cb9cccc850a
Onsi Fakhouri <onsijoe@gmail.com>
2022-09-19 11:39:52 -0600

    cover the entire gmeasure suite with leak detection

--- gmeasure/gmeasure_suite_test.go
+++ gmeasure/gmeasure_suite_test.go
@@ -5,9 +5,17 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gleak"
 )
 
 func TestGmeasure(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Gmeasure Suite")
 }
+
+var _ = BeforeEach(func() {
+	g := gleak.Goroutines()
+	DeferCleanup(func() {
+		Eventually(gleak.Goroutines).ShouldNot(gleak.HaveLeaked(g))
+	})
+})
